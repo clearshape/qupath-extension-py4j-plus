9e17552ad7a57d90df9531b6b7a95428c2987da9
clearshape <yaoting@gmail.com>
2025-04-16 07:58:11 +0800

    Create package-info.java

---
+++ src/main/java/qupath/ext/py4j/core/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains {@link QuPathEntryPoint2} to provide more useful methods
+ * on top of {@link qupath.ext.py4j.core.QuPathEntryPoint QuPathEntryPoint}.
+ */
+package qupath.ext.py4j.core;
